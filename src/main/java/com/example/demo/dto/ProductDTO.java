72ad809dabfb1ca32914afee0496b5bbb0fb0283
Danila Abramov <67269761+danilaabramov@users.noreply.github.com>
2023-07-19 16:09:38 +0400

    Add files via upload

---
+++ src/main/java/com/example/demo/dto/ProductDTO.java
@@ -0,0 +1,10 @@
+package com.example.demo.dto;
+
+import lombok.Data;
+
+@Data
+public class ProductDTO {
+
+    private String name;
+    private Short amount;
+}
---
+++ src/main/java/com/example/demo/entity/Product.java
@@ -0,0 +1,20 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private Long id;
+    private String name;
+    private Short amount;
+}
